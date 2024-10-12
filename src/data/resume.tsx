4a1b20b2ebd8c034956dfe3bfdd17b84c3b97554
Mohammed Ahteshamuz Zaman <ahteshamzaman2024@gmail.com>
2024-10-12 20:58:32 +0530

    Initial Commit 1

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Ahtesham Zaman",
+  name: "Zaman",
   initials: "Zaman",
   url: "https://dillion.io",
   location: "San Francisco, CA",
